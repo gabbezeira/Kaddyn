d85a91b7f70a8aade9ff744497328cedf90f4cae
Gabriel  Alves <68339733+GabrielWeises@users.noreply.github.com>
2022-09-14 09:09:26 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,3 +57,20 @@ function linkAction(){
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+let count = 1;
+document.getElementById("r1").checked = true;
+
+setInterval(function(){
+	nextImage();
+}, 2000)
+
+function nextImage() {
+	count++;
+	if(count>4){
+		count = 1;
+	}
+
+	document.getElementById("r"+count).checked = true;
+}
